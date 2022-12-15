84dbbfd2ccb054c568a302e4ee0025eb39635824
sabusai <sarabjitkaurg14@gmail.com>
2022-12-15 16:27:23 +0530

    Commit 1

---
+++ main.js
@@ -0,0 +1,67 @@
+// Create one function with zero parameter having a console statement;
+// function fun() {
+//     console.log("Hello world");
+// }
+// fun();
+
+// 2.Create one function which takes two values as a parameter and print the sum of them as "Sum of 3, 4 is 7"
+// function Sum(a, b) {
+//     console.log(a + b);
+// }
+// Sum(3,4)
+
+// 3.Create one arrow function
+// let a = ()=> {
+//     console.log("Hello...");
+// }
+// a();
+
+// 4."Print output: 
+// var x = 21;
+// var girl = function () {
+//     console.log(x);
+//     var x = 20;
+// };
+// girl ();
+// Ans undefined
+
+// 5."Print output: 
+// var x = 21;
+// girl ();
+// console.log(x)
+// function girl() {
+//     console.log(x);
+//     var x = 20;
+// };
+// Ans undefined 21
+
+// 6."Print output
+// var x = 21;
+// a();
+// b();
+
+// function a() {    
+//    x = 20;
+//   console.log(x);
+// };
+// function b() {    
+//     x = 40;
+//    console.log(x);
+// };
+// Ans 20 40
+
+// 7.Write a function that accepts parameter n and returns factorial of n
+// function factorial(n) {
+//     let answer = 1;
+//     if(n==0 || n == 1) {
+//         return answer
+//     } else {
+//         for(var i = n; i >=1; i--) {
+//             answer = answer * 1
+//         }
+//         return answer
+//     }
+// }
+// let n = 7;
+// let answer = factorial(n)
+// console.log(answer);
